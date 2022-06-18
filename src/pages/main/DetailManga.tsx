7005160f781ad2879b68b41bf1d2631c0a537967
Feri Teja Kusuma <feriteja@gmail.com>
2022-06-19 00:30:52 +0700

    add fav to comic

--- src/pages/main/DetailManga.tsx
+++ src/pages/main/DetailManga.tsx
@@ -6,13 +6,48 @@ import { getMangaDetail } from "../../services/manga";
 import { BsCircleFill, BsDownload, BsPen, BsPencil } from "react-icons/bs";
 import { HiOutlineDotsCircleHorizontal } from "react-icons/hi";
 import { MdExpandMore, MdExpandLess } from "react-icons/md";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import {
+  UserState,
+  userStateContextProps,
+} from "../../context/UserStateContext";
 
 const DetailManga = () => {
+  const { mangaList, addToFav, deleteFromFav } =
+    UserState() as userStateContextProps;
   const [showMore, setShowMore] = useState(false);
   const { title = "" } = useParams();
+  const [isFav, setIsFav] = useState(
+    mangaList.some((manga) => manga.endpoint === title)
+  );
+  const [isWaitFav, setIsWaitFav] = useState(false);
   const { status, data } = useQuery([`detail${title}`, title], () =>
     getMangaDetail(title)
   );
+
+  const handleFav = async () => {
+    if (isWaitFav) return;
+    setIsWaitFav(true);
+    try {
+      const dataManga = {
+        endpoint: data?.link.endpoint || "",
+        thumb: data?.thumb || "",
+        title: data?.title || "",
+      };
+      if (isFav) {
+        await deleteFromFav(dataManga);
+        setIsFav(false);
+        setIsWaitFav(false);
+        return;
+      }
+      await addToFav(dataManga);
+      setIsFav(true);
+
+      setIsWaitFav(false);
+    } catch (error) {
+      setIsWaitFav(false);
+    }
+  };
 
   if (status === "loading") return <SkeletonDetail />;
 
@@ -21,15 +56,26 @@ const DetailManga = () => {
       <div className="flex  w-full space-x-3  ">
         <img className="h-44 w-40 object-cover " src={data?.thumb} alt="" />
         <div className="flex flex-1 flex-col space-y-2 ">
-          <h1 className="font-bold text-2xl">{data?.title}</h1>
+          <div className="flex items-center justify-between pr-4 ">
+            <h1 className="font-bold text-2xl">{data?.title}</h1>
+            <button onClick={isFav ? handleFav : handleFav}>
+              {isFav ? (
+                <FaHeart size={25} className="text-pink-500" />
+              ) : (
+                <FaRegHeart size={25} />
+              )}
+            </button>
+          </div>
           <div className="flex items-center space-x-1">
             <HiOutlineDotsCircleHorizontal size={20} /> <h3>{data?.status}</h3>
           </div>
           <div className="flex flex-wrap">
             {"("}
-            {data?.alter.slice(0, Math.min(data.alter.length, 5)).map((e) => (
-              <span>{` ${e}`};</span>
-            ))}
+            {data?.alter
+              .slice(0, Math.min(data.alter.length, 5))
+              .map((alterName, idx) => (
+                <span key={alterName + idx}>{` ${alterName}`};</span>
+              ))}
             {")"}
           </div>
           <div className="flex flex-col md:flex-row md:space-x-3 ">
@@ -42,7 +88,9 @@ const DetailManga = () => {
           </div>
           <div className="flex space-x-3 ">
             {data?.genre.map((genre) => (
-              <h3 className="font-semibold">{genre.name}</h3>
+              <h3 key={genre.name} className="font-semibold">
+                {genre.name}
+              </h3>
             ))}
           </div>
         </div>
@@ -78,7 +126,11 @@ const DetailManga = () => {
       <div className="flex flex-col space-y-2 overflow-auto max-h-[500px] scrollbar-hide">
         {data?.chapters.map((chapter) => {
           return (
-            <Link to={`/komik/${title}/${chapter.endpoint}`}>
+            <Link
+              key={chapter.endpoint + "chapterManga"}
+              className="visited:opacity-75"
+              to={`/komik/${title}/${chapter.endpoint}`}
+            >
               <div className="flex justify-between px-2 py-2 rounded-md shadow-sm bg-secondary">
                 <h2>{chapter.title}</h2>
                 <BsDownload />
