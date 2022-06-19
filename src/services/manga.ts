fd52d4d289249d45cd771dbc89eb6cdd87214b2b
Feri Teja Kusuma <feriteja@gmail.com>
2022-06-19 20:11:13 +0700

    delete download manga func

--- src/services/manga.ts
+++ src/services/manga.ts
@@ -38,16 +38,4 @@ const getMangaChapter = async (endPoint: string) => {
   }
 };
 
-const getMangaFile = async (downloadLine: string) => {
-  try {
-    const uri = `${baseUrl}/download/${downloadLine}`;
-    console.log(uri);
-    const res = await axios.get(uri, {
-      responseType: "blob",
-    });
-    return res;
-  } catch (error) {
-    throw { error };
-  }
-};
-export { getMenuManga, getMangaDetail, getMangaChapter, getMangaFile };
+export { getMenuManga, getMangaDetail, getMangaChapter };
